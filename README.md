fd9042c0b5f1e3c7048906df1ffd2bd93c86afdb
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-25 12:41:11 +0200

    Add resources with only: keyword

--- README.md
+++ README.md
@@ -29,7 +29,5 @@ Things you may want to cover:
     user can sign up
     user can log out
     user can add a article
-    user can edit his article
-    user can delete his article
 - [ ] Posts => models, controllers
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_095530) do
+ActiveRecord::Schema.define(version: 2020_08_25_100531) do
 
   create_table "articles", force: :cascade do |t|
-    t.string "body"
+    t.text "body"
     t.integer "users_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["users_id"], name: "index_articles_on_users_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "articles", "users", column: "users_id"
